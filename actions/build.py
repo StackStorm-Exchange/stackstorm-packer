b2b5066018305954f36417872ccb3ce294bc468d
Bradley Bishop <bradley.bishop@encore.tech>
2019-01-26 20:05:12 -0500

    Added a try except block so that if the build fails then all the data from the packer build is still displayed just like if the build was a success. Also with the addition of inspec tests there was an error reading the output because of all the ansi characters from the command line. So we now grab that output and drop all those characters and just return the text. Results are returned in the same structure as before with a dictionary of a status and a result.

--- actions/build.py
+++ actions/build.py
@@ -1,4 +1,5 @@
 from lib.actions import BaseAction
+import re
 
 
 class BuildAction(BaseAction):
@@ -9,4 +10,36 @@ class BuildAction(BaseAction):
 
         p = self.packer(packerfile, exc=exclude, only=only, variables=variables,
                         vars_file=variables_file)
-        return p.build(parallel=parallel, debug=debug, force=force)
+
+        # If the packer build method fails. It only shows a small amount of data that
+        # is not always helpful depending on where the error is. This way all the packer
+        # information will be given the same as if the build succeeded
+        result = None
+        try:
+            p_result = p.build(parallel=parallel, debug=debug, force=force)
+            result = self.format_results(p_result.stdout)
+        except Exception as e:
+            result = (False, self.format_results(e.stdout, True))
+
+        return result
+
+    def format_results(self, result_string, error=False):
+        '''When returning the packer output information that are
+        ansi characters that do not convert properly. Also when output
+        the content to a string ansi escapes are added also. We need to
+        convert these charaters if they exist and drop all the escapes
+        so it outputs properly to stakcstorm.
+        '''
+        # ansi escape sequence
+        ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+
+        # Drop all the ansi escapes and return the results
+        result_string = ansi_escape.sub('', result_string)
+
+        # Return a success or error status
+        result_status = "success"
+        if error:
+            result_status = "error"
+
+        # Return dictionary with status and result
+        return {'status': result_status, 'result': result_string}
