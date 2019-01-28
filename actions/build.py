5020293da3f7e68eb651b1cefca3c6512861b556
Bradley Bishop <bradley.bishop@encore.tech>
2019-01-28 15:17:25 -0500

    Trying to fix tests.

--- actions/build.py
+++ actions/build.py
@@ -3,25 +3,6 @@ import re
 
 
 class BuildAction(BaseAction):
-    def run(self, packerfile, cwd=None, exclude=None, only=None, variables=None,
-            variables_file=None, parallel=True, debug=False, force=False):
-        if cwd:
-            self.set_dir(cwd)
-
-        p = self.packer(packerfile, exc=exclude, only=only, variables=variables,
-                        vars_file=variables_file)
-
-        # If the packer build method fails. It only shows a small amount of data that
-        # is not always helpful depending on where the error is. In the except block
-        # we grab the whole output and return it.
-        result = None
-        try:
-            p_result = p.build(parallel=parallel, debug=debug, force=force)
-            result = self.format_results(p_result.stdout)
-        except Exception as e:
-            result = (False, self.format_results(e.stdout, True))  # pylint: disable=no-member
-
-        return result
 
     def format_results(self, result_string, error=False):
         '''When returning the packer output information that are
@@ -43,3 +24,23 @@ class BuildAction(BaseAction):
 
         # Return dictionary with status and result
         return {'status': result_status, 'result': result_string}
+
+    def run(self, packerfile, cwd=None, exclude=None, only=None, variables=None,
+            variables_file=None, parallel=True, debug=False, force=False):
+        if cwd:
+            self.set_dir(cwd)
+
+        p = self.packer(packerfile, exc=exclude, only=only, variables=variables,
+                        vars_file=variables_file)
+
+        # If the packer build method fails. It only shows a small amount of data that
+        # is not always helpful depending on where the error is. In the except block
+        # we grab the whole output and return it.
+        result = None
+        try:
+            p_result = p.build(parallel=parallel, debug=debug, force=force)
+            result = self.format_results(p_result.stdout)
+        except Exception as e:
+            result = (False, self.format_results(e.stdout, True))  # pylint: disable=no-member
+
+        return result
